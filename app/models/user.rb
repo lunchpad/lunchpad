04412f6d0328a7e934214574291b259688275d08
Nick Passarella <nickpassarella@gmail.com>
2014-11-16 18:00:21 -0500

    added more specificity to email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ActiveRecord::Base
   has_many :orders
   has_many :accounts
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
-            format: { with: /\A[\w\-\.]+@[\w\-\.]+\Z/, message: "must be a valid email address" }
+            format: { with: VALID_EMAIL_REGEX, message: "must be a valid email address" }
 
   has_secure_password
 end
