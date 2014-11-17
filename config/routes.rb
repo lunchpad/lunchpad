ba6eca1cb585b6548e94df0862c0a8259cd1c03f
Kheang Lim <kheang@gmail.com>
2014-11-16 23:29:33 -0500

    Added vendor and menu items controller; added vendor controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :vendors do
+    resources :menu_items, except: [:index]
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
