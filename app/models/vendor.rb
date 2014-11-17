3e199c77c8420f1f5829c23308ac3e03ec99b199
Kheang Lim <kheang@gmail.com>
2014-11-16 19:14:48 -0500

    Removed requirement of email and phone number

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -6,11 +6,9 @@ class Vendor < ActiveRecord::Base
             uniqueness: { case_sensitive: false }
 
   validates :email,
-            presence: true,
             uniqueness: { case_sensitive: false },
             format: { with: /\A[\w\-\.]+@[\w\-\.]+\Z/, message: 'must be a valid email address' }
 
   validates :phone_number,
-            presence: true,
             format: { with: /\A\d{3}-\d{3}-\d{4}\Z/, message: 'must be formatted as ###-###-####' }
 end
