aa4ff16b7104c49b70d86e2925f996a73e20238b
Kheang Lim <kheang@gmail.com>
2014-11-17 23:32:55 -0500

    corrected ordered_items fixtures

--- test/models/ordered_item_test.rb
+++ test/models/ordered_item_test.rb
@@ -9,4 +9,15 @@ class OrderedItemTest < ActiveSupport::TestCase
   should validate_presence_of(:menu_item_id)
   should validate_presence_of(:account_id)
   should validate_presence_of(:delivery_date)
+
+  # context 'Ordered Item class' do
+  #   should 'be able to query for all orders that have not been submitted' do
+  #     ordered_items = OrderedItem.not_submitted
+  #     assert_includes ordered_items, ordered_items(:one)
+  #
+  #     ordered_items.each do |ordered_item|
+  #       assert_not ordered_item.submitted
+  #     end
+  #   end
+  # end
 end
