b27fe9a1a08a23f252a831a427096a0f3b4a0d13
Nick Passarella <nickpassarella@gmail.com>
2014-11-20 14:48:28 -0500

    cleaned out unneeded commented code

--- app/controllers/ordered_items_controller.rb
+++ app/controllers/ordered_items_controller.rb
@@ -20,21 +20,8 @@ class OrderedItemsController < ApplicationController
                                     quantity: @quantity)
 
     end
-    # binding.pry
-    # # @orders_not_submitted.each do |key, value|
-    # #   hash = params.require(key).permit(:quantity)
-    # #   value.update(quantity: hash[:quantity].to_i)
-    # #   value.save
-    # # end
-    # params[:order].each do |ami_id,quantity|
-    #   p ami_id.to_i
-    #   p quantity.to_i
-    # end
-    binding.pry
+
     redirect_to ordered_items_path
-
-    # post :create, { ami_id: "113629430", quantity: 1 }, { ami_id: "298486374", quantity: 2 }
-
   end
 
   def update
