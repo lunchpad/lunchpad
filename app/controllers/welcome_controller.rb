4676fc77569a917809d79105dfd5bd09ef54f25e
Nick Passarella <nickpassarella@gmail.com>
2014-11-22 18:37:39 -0500

    added bourbon/bitters. dropped in some basic styling to neaten up views.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,17 @@
 class WelcomeController < ApplicationController
 
-  def index
+  before_action :set_date_range
 
+  def index
+  end
+
+  private
+
+  def set_date_range
+    monday  = Date.parse("Monday")
+    delta = monday > Date.today ? 0 : 7
+    @start_date = (monday + delta)
+    @end_date = @start_date + 5
   end
 
 end
