f1c09bc7f731fe6b49e417783dbf6cba573021e4
DeeDee Lavinder <deedeelavinder@gmail.com>
2014-11-23 16:59:27 -0500

    set up precompiling assests, tweaked devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  # devise_for :users, :controllers => { registrations: 'registrations' }
 
 
   resources :accounts, only: [:new, :create, :show, :edit, :update] do
