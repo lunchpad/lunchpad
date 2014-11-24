5f8ce49e66145bd7049fba0062d22b7b69468674
Kheang Lim <kheang@gmail.com>
2014-11-24 00:28:57 -0500

    edited javascript to stop page from scrolling up on click for quantity

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -3,11 +3,13 @@ $( document ).ready(function() {
         var index = this.getAttribute('data-item');
         var quantity = document.getElementById('order_ordered_items_attributes_' + index + '_quantity');
         quantity.value++;
+        return false;
     });
 
     $('.quantity_subtract').click(function () {
         var index = this.getAttribute('data-item');
         var quantity = document.getElementById('order_ordered_items_attributes_' + index + '_quantity');
         quantity.value = Math.max((quantity.value - 1), 0);
+        return false;
     });
 });
