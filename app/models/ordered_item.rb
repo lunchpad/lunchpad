294d4a3c78ac69bfd62e2096fd17cb2c53c38853
Kheang Lim <kheang@gmail.com>
2014-11-23 23:34:24 -0500

    cleaned up views

--- app/models/ordered_item.rb
+++ app/models/ordered_item.rb
@@ -2,6 +2,7 @@ class OrderedItem < ActiveRecord::Base
   belongs_to :available_menu_item
   belongs_to :order
   delegate :menu_item, :to => :available_menu_item, :allow_nil => true
+  delegate :date, :to => :available_menu_item, :allow_nil => true
 
   validates :available_menu_item_id,
             presence: true
