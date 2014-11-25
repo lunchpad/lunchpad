c3d453d17165a1705a03fa369a141125b5353889
Kheang Lim <kheang@gmail.com>
2014-11-24 23:15:38 -0500

    Adjust account balances after creating and updating orders

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -14,21 +14,15 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   def valid_items_attributes
-    { '1' => { quantity: 1,
-               available_menu_item_id: available_menu_items(:one).id },
-      '2' => { quantity: 2,
-               available_menu_item_id: available_menu_items(:two).id},
-      '3' => { quantity: 0,
-               available_menu_item_id: available_menu_items(:three).id } }
+    { '1' => { quantity: 1, available_menu_item_id: available_menu_items(:one).id },
+      '2' => { quantity: 2, available_menu_item_id: available_menu_items(:two).id},
+      '3' => { quantity: 0, available_menu_item_id: available_menu_items(:three).id } }
   end
 
   def invalid_items_attributes
-    { '1' => { quantity: '',
-               available_menu_item_id: '' },
-      '2' => { quantity: '',
-               available_menu_item_id: '' },
-      '3' => { quantity: '',
-               available_menu_item_id: '' } }
+    { '1' => { quantity: '', available_menu_item_id: '' },
+      '2' => { quantity: '', available_menu_item_id: '' },
+      '3' => { quantity: '', available_menu_item_id: '' } }
   end
 
   context 'GET orders#index' do
@@ -63,6 +57,64 @@ class OrdersControllerTest < ActionController::TestCase
 
     should 'load order' do
       assert assigns[:order], 'Should load order'
+    end
+  end
+
+  context 'GET orders#edit' do
+    setup { get :edit, { account_id: accounts(:one).id, id: orders(:one).id } }
+
+    should render_template('edit')
+    should respond_with(:success)
+
+    should 'load order' do
+      assert assigns[:order], 'Should load order'
+    end
+  end
+
+  context 'GET orders#update' do
+    context 'when user enters valid attributes' do
+      setup do
+        updated_items_attributes = { '1' => { quantity: 2,
+                                              available_menu_item_id: available_menu_items(:one).id,
+                                              id: ordered_items(:one).id },
+                                     '2' => { quantity: 2,
+                                              available_menu_item_id: available_menu_items(:two).id,
+                                              id: ordered_items(:two).id } }
+        patch :update, { account_id: accounts(:one).id,
+                         id: orders(:one).id,
+                         order: { ordered_items_attributes: updated_items_attributes } }
+      end
+
+      should 'redirect to order show' do
+        assert_redirected_to account_order_path
+      end
+
+      should 'save order' do
+        assert_saved_model(:order)
+      end
+
+      should 'update quantity of items' do
+        assert_equal 2, assigns[:order].ordered_items.first.quantity
+        assert_equal 2, assigns[:order].ordered_items.second.quantity
+      end
+    end
+
+    context 'when user does not enter valid attributes' do
+      setup do
+        updated_items_attributes = { '1' => { quantity: '',
+                                              available_menu_item_id: '',
+                                              id: ordered_items(:one).id },
+                                     '2' => { quantity: '',
+                                              available_menu_item_id: '',
+                                              id: ordered_items(:two).id } }
+        patch :update, { account_id: accounts(:one).id,
+                         id: orders(:one).id,
+                         order: { ordered_items_attributes: updated_items_attributes } }
+      end
+
+      should 'redirect to order show' do
+        assert_redirected_to edit_account_order_path
+      end
     end
   end
 
--- test/models/ordered_item_test.rb
+++ test/models/ordered_item_test.rb
@@ -14,5 +14,20 @@ class OrderedItemTest < ActiveSupport::TestCase
 
       assert_equal 20, ordered_item.subtotal, 'should get array of subtotals for '
     end
+
+    should 'after_create, update account balance with order subtotal' do
+      assert_difference 'accounts(:one).reload.balance',10 do
+        order = Order.create(account: accounts(:one))
+        order.ordered_items.create(available_menu_item: available_menu_items(:one), quantity: 1)
+      end
+    end
+
+    should 'after_update, update account balance with order subtotal' do
+      assert_difference 'accounts(:one).reload.balance',30 do
+        order = Order.create(account: accounts(:one))
+        order.ordered_items.create(available_menu_item: available_menu_items(:one), quantity: 1)
+        order.ordered_items.first.update(quantity: 3)
+      end
+    end
   end
 end
