bc086bfecb7425269db73306ab8fbc08d685df73
DeeDee Lavinder <deedeelavinder@gmail.com>
2014-11-30 15:41:25 -0500

    moved lunch_coverage logic from view to model

--- app/models/order.rb
+++ app/models/order.rb
@@ -21,4 +21,30 @@ class Order < ActiveRecord::Base
   def begin_date
     ordered_items.first.date
   end
+
+  def lunch_coverage
+    items_ordered = ordered_items.select {|oi| oi["quantity"] > 0 }
+    days = items_ordered.map(&:date).uniq
+  end
+
+  # def ordered
+  #   ordered_items.select {|oi| oi["quantity"] > 0 }
+  # end
+  #
+  # def map_coverage
+  #   days = ordered.map(&:date).uniq
+  #   days.each do |d|
+  #     d.strftime("%A")
+  #   end
+  # end
+
 end
+
+# <% a = account.has_order_for(@start_date) %>
+# <% if a.present? %>
+#       <% lc = a.ordered_items.select {|oi| oi["quantity"] > 0 } %>
+#   <% days = lc.map(&:date).uniq %>
+#       <% days.each do |d| %>
+# <%= d.strftime("%A") %>
+#         <% end %>
+#     <% end %>
