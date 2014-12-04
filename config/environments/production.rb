025c734fd3d967e752a8eb5220910450cd8d2050
Nick Passarella <nickpassarella@gmail.com>
2014-12-04 15:29:50 -0500

    temporarily fixing font awesome icons with CDN link until I can find a fix for the problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,9 +28,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.precompile += %w(
-    *.svg *.eot *.woff *.ttf *.png *.jpg *.jpeg *.gif
-  )
 
   # Generate digests for assets URLs.
   config.assets.digest = true
