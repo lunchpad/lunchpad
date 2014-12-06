e244917b56e25362cef715ee2ef65e37ac518a7e
Nick Passarella <nickpassarella@gmail.com>
2014-12-05 22:14:24 -0500

    commit before pulling and merging

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+
+  def find_ordered_items_for(account, start_date, end_date)
+    begin_datetime = start_date.beginning_of_day
+    end_datetime = end_date.end_of_day
+    OrderedItem.joins(:order, :available_menu_item).where('account_id = ? AND quantity > ? AND date > ? AND date < ?',account.id,0,begin_datetime,end_datetime)
+  end
+
+
 end
