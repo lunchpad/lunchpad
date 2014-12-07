084bf8b85fe9fc1ff46a4044732df6beb98667f0
DeeDee Lavinder <deedeelavinder@gmail.com>
2014-12-07 14:45:28 -0500

    working on stripe without checkout

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,7 +4,7 @@ class ChargesController < ApplicationController
 
   def create
     # Amount in cents
-    @amount = 500
+    @amount = params[:amount]
 
     customer = Stripe::Customer.create(
       :email => 'example@stripe.com',
@@ -23,3 +23,31 @@ class ChargesController < ApplicationController
     redirect_to charges_path
   end
 end
+
+
+
+# def create
+#   @amount = user.input
+#
+#   customer = Stripe::Customer.create(
+#     :email => 'user.email',
+#     :card  => params[:stripeToken]
+#   )
+#
+#   save_stripe_customer_id(user, customer.id)
+#
+#   charge = Stripe::Charge.create(
+#     :customer    => customer.id,
+#     :amount      => @amount,
+#     :description => 'Rails Stripe customer',
+#     :currency    => 'usd'
+#   )
+#
+# rescue Stripe::CardError => e
+#   flash[:error] = e.message
+#   redirect_to charges_path
+# end
+#
+# def save_stripe_custore_id
+#
+# end
