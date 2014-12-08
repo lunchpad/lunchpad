335c98e248ebed563e1ebce30f0e41d64ddc3cfe
DeeDee Lavinder <deedeelavinder@gmail.com>
2014-12-08 15:15:43 -0500

    altered devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'accounts#index'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
 
   resources :accounts, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
@@ -38,7 +40,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'accounts#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
